fac3ccefd1ce3fb8548d199708b802f4170af615
datingz <zy9712347@163.com>
2025-12-06 07:18:00 +0000

    small change

--- src/network_interface.cc
+++ src/network_interface.cc
@@ -35,43 +35,33 @@ void NetworkInterface::send_datagram( InternetDatagram dgram, const Address& nex
   // dsr MAC addr (dont'n know now)
   EthernetAddress MAC_dst;
 
-  uint32_t IP_dst =  next_hop.ipv4_numeric();
+  uint32_t IP_dst = next_hop.ipv4_numeric();
   // hit
-  if(this->arp_cache_.find(IP_dst) != this->arp_cache_.end()){
+  if ( this->arp_cache_.find( IP_dst ) != this->arp_cache_.end() ) {
     // set header
     MAC_dst = this->arp_cache_[IP_dst].MAC_address_;
     frame.header.type = EthernetHeader::TYPE_IPv4;
     frame.header.src = this->ethernet_address_;
     frame.header.dst = MAC_dst;
     // set payload
-    InternetDatagram d = dgram;
-    frame.payload = serialize( d );
-    this->transmit( frame );
-  } else{
-    // miss
-    // if(this->datagrams_queued_.find(IP_dst) != this->datagrams_queued_.end()){
-    //   // do not resend but push in queue
-    //   DatagramState d;
-    //   d.dgram_ = dgram;
-    //   d.timestamp_ = 0;
-    //   this->datagrams_queued_.emplace( IP_dst, d);
-    // }
-    
+    frame.payload = serialize( std::move( dgram ) );
+    this->transmit( std::move( frame ) );
+  } else {
+
     // should resend?
-    auto range = this->datagrams_queued_.equal_range(IP_dst);
+    auto range = this->datagrams_queued_.equal_range( IP_dst );
     bool do_not_resend = false;
-    for(auto it = range.first; it != range.second; ++it){
-      if(it->second.had_resent_){
+    for ( auto it = range.first; it != range.second; ++it ) {
+      if ( it->second.had_resent_ ) {
         do_not_resend = true;
       }
     }
-
     // push in queue
     DatagramState d;
     d.dgram_ = dgram;
     d.timestamp_ = 0;
-    if(do_not_resend){
-      this->datagrams_queued_.emplace( IP_dst, d );
+    if ( do_not_resend ) {
+      this->datagrams_queued_.emplace( IP_dst, std::move( d ) );
       return;
     }
     d.had_resent_ = true;
@@ -80,24 +70,17 @@ void NetworkInterface::send_datagram( InternetDatagram dgram, const Address& nex
     ARPMessage arp_msg;
     // set arp message
     arp_msg.opcode = ARPMessage::OPCODE_REQUEST;
-    arp_msg.sender_ethernet_address =this->ethernet_address_;
+    arp_msg.sender_ethernet_address = this->ethernet_address_;
     arp_msg.sender_ip_address = this->ip_address_.ipv4_numeric();
-    // arp_msg.target_ethernet_address = ETHERNET_BROADCAST;
     arp_msg.target_ip_address = IP_dst;
-    // // push in queue
-    // DatagramState d;
-    // d.dgram_ = dgram;
-    // d.timestamp_ = 0;
-    // this->datagrams_queued_.emplace( IP_dst, d );
-    // set frame header
+    // set header
     frame.header.type = EthernetHeader::TYPE_ARP;
     frame.header.src = this->ethernet_address_;
     frame.header.dst = ETHERNET_BROADCAST;
     // set frame payload
     frame.payload = serialize( arp_msg );
-    this->transmit( frame );
+    this->transmit( std::move( frame ) );
   }
-  
 }
 
 //! \param[in] frame the incoming Ethernet frame
@@ -105,20 +88,20 @@ void NetworkInterface::recv_frame( EthernetFrame frame )
 {
   // unrelated situlation
   EthernetAddress target = frame.header.dst;
-  if(target != ETHERNET_BROADCAST && target != this->ethernet_address_){
+  if ( target != ETHERNET_BROADCAST && target != this->ethernet_address_ ) {
     return;
   }
 
   uint16_t type = frame.header.type;
-  if(type == EthernetHeader::TYPE_IPv4){
+  if ( type == EthernetHeader::TYPE_IPv4 ) {
     InternetDatagram dgram;
-    if(parse( dgram, frame.payload ) != true){
+    if ( parse( dgram, frame.payload ) != true ) {
       return;
     }
     this->datagrams_received_.push( dgram );
-  } else if(type == EthernetHeader::TYPE_ARP){
+  } else if ( type == EthernetHeader::TYPE_ARP ) {
     ARPMessage arp_msg;
-    if(parse( arp_msg, frame.payload ) != true){
+    if ( parse( arp_msg, frame.payload ) != true ) {
       return;
     }
     // learn new cast
@@ -127,22 +110,13 @@ void NetworkInterface::recv_frame( EthernetFrame frame )
     ArpCacheState a;
     a.MAC_address_ = MAC_learn;
     a.timestamp_ = 0;
-    this->arp_cache_[IP_learn] = a;
+    this->arp_cache_[IP_learn] = std::move( a );
 
     // send: resend dgram or send reply
     EthernetFrame send;
     // if find in queue, resend the dgram
     auto range = this->datagrams_queued_.equal_range( IP_learn );
-    // if(this->datagrams_queued_.find(IP_learn) != this->datagrams_queued_.end()){
-    //   send.header.type = EthernetHeader::TYPE_IPv4;
-    //   send.header.src = this->ethernet_address_;
-    //   send.header.dst = MAC_learn;
-    //   InternetDatagram d = this->datagrams_queued_[IP_learn].dgram_;
-    //   send.payload = serialize( d );
-    //   this->datagrams_queued_.erase(IP_learn);
-    //   this->transmit( send );
-    // }  
-    for(auto it = range.first; it != range.second; ++it){
+    for ( auto it = range.first; it != range.second; ++it ) {
       send.header.type = EthernetHeader::TYPE_IPv4;
       send.header.src = this->ethernet_address_;
       send.header.dst = MAC_learn;
@@ -151,8 +125,8 @@ void NetworkInterface::recv_frame( EthernetFrame frame )
       this->transmit( send );
     }
     this->datagrams_queued_.erase( IP_learn );
-    if(arp_msg.opcode == ARPMessage::OPCODE_REQUEST){
-      if(arp_msg.target_ip_address != this->ip_address_.ipv4_numeric()){
+    if ( arp_msg.opcode == ARPMessage::OPCODE_REQUEST ) {
+      if ( arp_msg.target_ip_address != this->ip_address_.ipv4_numeric() ) {
         return;
       }
       // if receive arp request, relpy it
@@ -168,34 +142,28 @@ void NetworkInterface::recv_frame( EthernetFrame frame )
       send.header.dst = MAC_learn;
       // set payload
       send.payload = serialize( new_arp_msg );
-      this->transmit( send );
+      this->transmit( std::move( send ) );
     }
-    
   }
 }
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
 void NetworkInterface::tick( const size_t ms_since_last_tick )
 {
-  for(auto it = this->arp_cache_.begin(); it != this->arp_cache_.end();){
+  for ( auto it = this->arp_cache_.begin(); it != this->arp_cache_.end(); ) {
     it->second.timestamp_ += ms_since_last_tick;
-    if(it->second.timestamp_ > 30000){
-      it = this->arp_cache_.erase(it);
-    } else{
+    if ( it->second.timestamp_ > 30000 ) {
+      it = this->arp_cache_.erase( it );
+    } else {
       it++;
     }
-    
   }
-  for(auto it = this->datagrams_queued_.begin(); it != this->datagrams_queued_.end();){
+  for ( auto it = this->datagrams_queued_.begin(); it != this->datagrams_queued_.end(); ) {
     it->second.timestamp_ += ms_since_last_tick;
-    if(it->second.timestamp_ > 5000){
-      it = this->datagrams_queued_.erase(it);
-    } else{
+    if ( it->second.timestamp_ > 5000 ) {
+      it = this->datagrams_queued_.erase( it );
+    } else {
       it++;
     }
-    
   }
 }
-
-
-
